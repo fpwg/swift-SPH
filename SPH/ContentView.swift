dd59fa827d0557b8000f610adb1ab94524a55c8c
fpwg <46819544+fpwg@users.noreply.github.com>
2024-12-06 13:09:48 +0100

    Initial Commit

---
+++ SPH/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SPH
+//
+//  Created by Florian Plaswig on 06.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SPH/SPHApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPHApp.swift
+//  SPH
+//
+//  Created by Florian Plaswig on 06.12.24.
+//
+
+import SwiftUI
+
+@main
+struct SPHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
